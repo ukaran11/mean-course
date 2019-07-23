0f40f46840722af91560b12fe6bea30642837d7c
Utsav <ukaran11@gmail.com>
2019-07-23 11:21:06 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -33,29 +33,29 @@ app.post("/api/posts", (req, res, next) => {
         title: req.body.title,
         content: req.body.content
     })
-    console.log(post)
-    post.save()
-    res.status(201).json({
-        message: 'Post added successfully'
+    post.save().then(createdPost => {
+        res.status(201).json({
+            message: 'Post added successfully',
+            postId: createdPost._id
+        })
     })
+   
 })
 
 app.get("/api/posts",(req, res, next) => {
-    const posts = [
-        {
-            id:"fdsafas",
-            title:"First server-side post",
-            content:"This is coming from the server"
-        },
-        {
-            id:"fewefwf",
-            title:"Second server-side post",
-            content:"This is coming from the server"
-        }
-    ];
-    res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts:posts
+    Post.find().then(documents => {
+        res.status(200).json({
+            message: 'Posts fetched successfully!',
+            posts:documents 
+    })
+    
+    })
+})
+
+app.delete("/api/posts/:id", (req, res, next) => {
+    Post.deleteOne({_id:req.params.id}).then(result => {
+        console.log(result)
+        res.status(200).json({message:"Post deleted!"});
     })
 })
 // Password db - 5COgYYQpifbYJi4k
